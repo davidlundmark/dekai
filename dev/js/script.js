5c27ca3c9148d0bbc3e5272a71539bc478315372
David Lundmark <davelundmark@gmail.com>
2016-08-26 20:15:51 +0200

    fix
    fix

--- dev/js/script.js
+++ dev/js/script.js
@@ -1,8 +1,32 @@
 var $ = require('jquery');
-//$('p').html('New text');
 
-console.log('Module log');
+console.log('deKai v.1');
 
+//#region TouchHandler
+var TouchHandler = function () { }
+
+TouchHandler.prototype = {
+    isMobile: null,
+    isTouch: null,
+    init: function () {
+        this.checkDevice();
+        (this.isMobile) ? $('body').addClass('has-touch') : $('body').addClass('no-touch');
+    },
+
+    checkDevice: function () {
+        this.isMobile = navigator.userAgent.match(/(iPad)|(iPhone)|(iPod)|(Android)|(PlayBook)|(BB10)|(BlackBerry)|(Opera Mini)|(IEMobile)|(webOS)|(MeeGo)/i);
+        this.isTouch = this.isMobile !== null || document.createTouch !== undefined || 'ontouchstart' in window || 'onmsgesturechange' in window || navigator.msMaxTouchPoints;
+    }
+};
+
+var touchHandler = new TouchHandler();
+
+$(document).ready(function () {
+    touchHandler.init();
+});
+//#endregion
+
+/*
 $(document).ready(function () {
   $('.accordion-tabs').each(function(index) {
     $(this).children('li').first().children('a').addClass('is-active').next().addClass('is-open').show();
@@ -21,11 +45,12 @@ $(document).ready(function () {
     }
   });
 });
-
+*/
 $(document).ready(function(){
   $('.js-menu-trigger').on('click touchstart',function (e) {
     $('.side-nav').toggleClass('is-visible');
     e.preventDefault();
+    return false;
   });
 });
 
@@ -33,5 +58,7 @@ $('.js-accordion-trigger').bind('click', function(e){
   $(this).parent().find('.submenu').slideToggle('fast');  // apply the toggle to the ul
   $(this).parent().toggleClass('is-expanded');
   e.preventDefault();
+  return false;
 });
+
 
